fad9d0fdbc4909762813225e59024fe154899737
Sergio Arias Fernández <166837201+mwolft@users.noreply.github.com>
2025-04-29 15:39:35 +0000

    Fix: Remove debug mode for production

--- src/app.py
+++ src/app.py
@@ -62,5 +62,5 @@ def serve_any_other_file(path):
 
 # Ejecutar la app
 if __name__ == '__main__':
-    PORT = int(os.environ.get('PORT', 3001))
-    app.run(host='0.0.0.0', port=PORT, debug=True)
+    PORT = int(os.environ.get('PORT', 3000))
+    app.run(host='0.0.0.0', port=PORT)
