e0f7bf229015741468ad21f3bcefde54c10830bf
Sergio Arias Fernández <166837201+mwolft@users.noreply.github.com>
2025-05-14 11:41:29 +0000

    eee

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  root: './src',
+  root: 'src',
   build: {
-    outDir: '../dist'
+    outDir: '../dist',
+    emptyOutDir: true
   },
-  server: {
-    port: 3000
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    }
   }
-})
+});
